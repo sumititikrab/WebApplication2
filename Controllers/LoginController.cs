4bedeb262079c86bd2dbfffa53b2edb0c2f44710
Sumit Kumar Singh <sumitit.ikrab@gmail.com>
2021-05-20 17:23:42 +0530

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BussinessLogic;
+using BussinessObject;
+
+namespace WebApplication2.Controllers
+{
+    public class LoginController : Controller
+    {
+        private TransactionMaster transactionMaster = new TransactionMaster();
+        // GET: Login
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "UserName,Password")] UserMaster userMaster)
+        {
+            try
+            {
+                string user= transactionMaster.UserLogin(userMaster);
+                if (string.IsNullOrEmpty(user))
+                {
+                    ViewBag.SucessMessage = "Welcome "+user;
+                    return View();
+                }
+                else
+                {
+                    ViewBag.SucessMessage = "Welcome sumit";
+                    //ViewBag.ErrorMessage = "Please Check!";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.ToString();
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/UserRegistrationController.cs
@@ -0,0 +1,47 @@
+﻿using BussinessObject;
+using BussinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+        private TransactionMaster transactionMaster = new TransactionMaster();
+
+        // GET: UserRegistration
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "UserID,UserName,EmailId,Phone,Password,ConfirmPassword")] UserMaster userMaster)
+        {
+            try
+            {
+                if (ModelState.IsValid && userMaster.Password == userMaster.ConfirmPassword)
+                {
+                    userMaster.UserID = Guid.NewGuid();
+                    transactionMaster.UserRegistration(userMaster);
+                    ViewBag.SucessMessage = "Registration Sucessfully Done!";
+                    return View();
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Please Check!";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.ToString();
+                return View();
+            }
+
+        }
+    }
+}
